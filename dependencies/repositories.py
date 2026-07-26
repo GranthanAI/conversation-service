a1f0e7b7486e9725f25a50d2e9ef01d9bf84e10d
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2026-07-26 20:52:29 +0530

    Delete clean architecture folder structure and revert repository

--- dependencies/repositories.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Depends
-from dependencies.database import get_cassandra_manager, CassandraClientManager
-from infrastructure.cassandra.conversation_repository import CassandraConversationRepository
-
-def get_conversation_repository(
-    manager: CassandraClientManager = Depends(get_cassandra_manager)
-) -> CassandraConversationRepository:
-    return CassandraConversationRepository(client_manager=manager)
